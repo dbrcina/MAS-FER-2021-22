1b77c94b668a0c2f08bb7000cd02b6229b0b33b6
dbrcina <dbrcina@donotreply.com>
2021-10-25 18:47:33 +0200

    wip

--- dz1/src/darijo_brcina_dz1.c
+++ dz1/src/darijo_brcina_dz1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, const char* argv)
-{
-	printf("Hello, World!\n");
-	return EXIT_SUCCESS;
+int main(int argc, const char *argv[]) {
+    if (argc != (1 + 3)) {
+        fprintf(stderr,
+                "Program expects path to some .ppm image file, block number and output file for storing the results!\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *fptr = fopen(argv[1], "r");
+    if (fptr == NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    fclose(fptr);
+    return EXIT_SUCCESS;
 }
